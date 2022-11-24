48be8fbdbec7a581ee5825c4ab4eb6ae91491f43
swon95 <shimtkddnjs13@gmail.com>
2022-11-24 19:39:24 +0900

    [FEAT] 20221124_Cartpage-UI

--- React/project/src/pages/CartPage/CartPage.jsx
+++ React/project/src/pages/CartPage/CartPage.jsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Clayful from 'clayful/client-js'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 function CartPage() {
+    const [cart, setCart] = useState({})
+    const navigete = useNavigate()
     
     useEffect(() => {
         var Cart = Clayful.Cart
@@ -18,13 +21,32 @@ function CartPage() {
             }
 
             var data = result.data
+            setCart(data.cart)
 
             console.log(data)
         })
     }, [])
+    
+    const items = cart.items
     return (
-        <div>
-            CartPage
+        <div className='pageWrapper'>
+            <div className='shopping-cart'>
+                <h1 className='title'>장바구니</h1>
+
+                <div className='shopping-cart-body' style={{ minHeight:100 }}></div>
+                
+                {items && items.length > 0 && (
+                    <div className='bottom'>
+                        <span className='total-prise'>
+                            총 금액: {cart.total?.amount.raw}{" "}
+                        </span>
+                        <button
+                            style={{ float : "right", padding: "4px 8px" }}
+                            onclick={() => Navigate('/payment')}>결제
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
