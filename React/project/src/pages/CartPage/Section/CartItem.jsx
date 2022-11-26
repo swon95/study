0b53d310db4cffed86d3e09d503df7cc9643c70b
swon95 <shimtkddnjs13@gmail.com>
2022-11-26 12:57:41 +0900

    [FEAT] 20221126_Cartpage-UI & Style

--- React/project/src/pages/CartPage/Section/CartItem.jsx
+++ React/project/src/pages/CartPage/Section/CartItem.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import './CartItem.css'
 
-function CartItem() {
+function CartItem({ item }) {
+  // props.item
   return (
-    <div>
-      CartItem
+    <div className='item'>
+
+      {/* 상품 이미지 영역 */}
+      <div className='image'>
+        <img style={{ height:'100%'}} src={item.product.thumbnail.url} alt={item.product.name} />
+      </div>
+
+      {/* 상품 설명 영역 */}
+      <div className='description'>
+        <span>{item.product.name}</span>
+        <span>Bball High</span>
+        <span>White</span>
+      </div>
+
+      {/* 상품 수량 영역 */}
+      <div className='quantity'>
+        <button className='plus-btn' type='button' name='button'>+</button>
+        <input type="text" readOnly name='name' value={item.quantity.raw} />
+        <button className='minus-btn' type='button' name='button'>-</button>
+      </div>
+
+      {/* 상품 금액 영역 */}
+      <div className='total-price'>₩{item.price.original.raw}</div>
+
+      {/* 상품 제거 영역 */}
+      <div className='buttons'>
+        <span className='delete-btn'>X</span>
+      </div>
     </div>
   )
 }
