34f7d9af6c472bd5999b0b64367454f197d9cf18
swon95 <shimtkddnjs13@gmail.com>
2023-01-03 20:00:59 +0900

    프로젝트 - 계산기 State 세팅(주석 추가)

--- React-Native/Project/Calculator/App.js
+++ React-Native/Project/Calculator/App.js
@@ -1,7 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // 사용자가 입력하는 값을 받는 state
+  const [input, setInput] = useState(0) // 12
+
+  // 입력받은 값을 기반으로 연산자를 선택했을때의 state
+  const [currentOperator, setCurrentOperator] = useState(null) // +
+
+  // 새로 입력받은 값 이전에 받았던 값을 저장하는 state
+  const [result, setResult] = useState(null) // 12 + 2 = 14
+
+  // 이전 계산에 사용됬던(입력받았던) 피연산자의 state 
+  const [tempInput, setTempInput] = useState(null) // 14
+
+  // temp 에 해당하는 값을 계산하기위한 연산자가 담긴 state
+  const [tempOperator, setTempOpearator] = useState(null) // 16, 18, 20, 22, 24.....
+
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
