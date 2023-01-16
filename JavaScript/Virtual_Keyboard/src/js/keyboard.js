2f281e003771bacf9df253b581952c053e9415c9
swon95 <shimtkddnjs13@gmail.com>
2023-01-16 19:49:59 +0900

    키보드 이벤트 추가

--- JavaScript/Virtual_Keyboard/src/js/keyboard.js
+++ JavaScript/Virtual_Keyboard/src/js/keyboard.js
@@ -4,6 +4,7 @@ export class Keyboard {
   #swichEl;
   #fontSelectEl;
   #containerEl;
+  #keyboardEl;
 
   constructor() {
     this.#assignElement();
@@ -16,17 +17,30 @@ export class Keyboard {
     // 하위 Element 들을 탐색 => 비용절감 효과
     this.#swichEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
   }
 
   #addEvent() {
     this.#swichEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
 
+    // 키보드를 눌렀을 때
     document.addEventListener("keydown", (event) => {
       console.log(event.code);
+      if (this.#keyboardEl.querySelector(`[data-code=${event.code}]`)) {
+        this.#keyboardEl
+          .querySelector(`[data-code=${event.code}]`)
+          .classList.add("active");
+      }
     });
+    // 누른 키보드를 뗏을 때
     document.addEventListener("keyup", (event) => {
       //   console.log("keyup");
+      if (this.#keyboardEl.querySelector(`[data-code=${event.code}]`)) {
+        this.#keyboardEl
+          .querySelector(`[data-code=${event.code}]`)
+          .classList.remove("active");
+      }
     });
   }
 
