0631ac36e19ed8f5dedbfa20bfd78cbd9b99f8f1
swon95 <shimtkddnjs13@gmail.com>
2023-03-25 02:17:05 +0900

    firebase inital commit

---
+++ React/project/slack-app/src/firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_XPHSy7EKMDUeucmWPLuC-dMM71PaB64",
+  authDomain: "slack-clone-1cfeb.firebaseapp.com",
+  databaseURL: "https://slack-clone-1cfeb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "slack-clone-1cfeb",
+  storageBucket: "slack-clone-1cfeb.appspot.com",
+  messagingSenderId: "783309607344",
+  appId: "1:783309607344:web:2f93f54002bbc0d5fb0675"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
