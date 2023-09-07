00912cbcc123c6ebb3bcacb9cea37bfc8c650dce
swon95 <shimtkddnjs13@gmail.com>
2023-09-08 01:22:29 +0900

    feat: card component

--- React/notepad/src/App.tsx
+++ React/notepad/src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from '@emotion/styled'
+import Card from './components/Card'
+
+const CardContainer = styled.div`
+  display: flex;
+  gap: 40px;
+  flex-wrap: wrap;
+  align-items: center;
+`
+
+const PlusCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 48px;
+  border: solid 1px #707070;
+  width: 80px;
+  height: 80px;
+  padding-bottom: 8px;
+  box-sizing: border-box;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <CardContainer>
+        <Card title='asd'/>
+        <Card title='asd'/>
+        <Card title='asd'/>
+        <Card title='asd'/>
+        <PlusCard>+</PlusCard>
+    </CardContainer>
   )
 }
 
